a3828256235209fdee896ece3fe9db1b66922b1d
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-19 21:53:08 +0200

    Added models for DB

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from src.database import db
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(debug=True)
