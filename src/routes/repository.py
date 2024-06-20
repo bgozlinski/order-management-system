53e40126f631bdf0b23cabd283b1bf62c1db7af0
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-20 10:02:53 +0200

    add API for get single order by id

--- src/routes/repository.py
+++ src/routes/repository.py
@@ -1,10 +1,11 @@
+from typing import List
 from src.database.models import Order
 from src.database.db import get_db
 from src.schemas.orders import OrderSchema
 from datetime import datetime
 
 
-def add_order(order: OrderSchema):
+def add_order(order: OrderSchema) -> Order:
     db = next(get_db())
     new_order = Order(
         name=order.name,
@@ -18,7 +19,20 @@ def add_order(order: OrderSchema):
     return new_order
 
 
-def get_orders():
+def get_orders() -> List[Order]:
     db = next(get_db())
     orders = db.query(Order).all()
     return orders
+
+
+def edit_order(id: int, updated_order: OrderSchema) -> Order:
+    db = next(get_db())
+    order = db.query(Order).get(id)
+    if order is None:
+        raise ValueError(f'Order {id} not found')
+    order.name = updated_order.name
+    order.description = updated_order.description
+    order.status = updated_order.status
+    db.commit()
+    db.refresh(order)
+    return order
