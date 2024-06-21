bb25922da56f8eee7e9f342ef57a591927e0bdb8
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-21 13:48:47 +0200

    Added dynamic database URL configuration with get_engine function

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
-from src.database import db
+
+from src.database.db import get_engine
 from src.database.models import Base
 from src.routes.endpoints import bp
+import os
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'sqlite:///:memory:'
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    engine = get_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    Base.metadata.create_all(bind=engine)
+
     app.register_blueprint(bp, url_prefix='/api')
-    Base.metadata.create_all(bind=db.engine)
+
     return app
 
 
