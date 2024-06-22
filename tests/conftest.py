8be6923a756579d47df18f820bf9a59ed8e7bef4
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-22 16:20:08 +0200

    conftest fix to test on testing DB

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,55 @@
 import pytest
 from src.app import create_app
-from src.database.db import get_engine, SessionLocal
+from src.database.db import get_engine, SessionLocal, get_db
 from src.database.models import Base
+from sqlalchemy.orm import sessionmaker
 
 
 @pytest.fixture(scope='function')
 def app():
     app = create_app('testing')
-    app.config.update({
-        "TESTING": True,
-    })
 
+    return app
+
+
+@pytest.fixture(scope='function')
+def engine(app):
+    """
+    Returns session-wide initialized database engine.
+    """
     engine = get_engine(app.config['SQLALCHEMY_DATABASE_URI'])
     SessionLocal.configure(bind=engine)
+    return engine
 
-    with app.app_context():
-        Base.metadata.create_all(bind=engine)
 
-    yield app
+@pytest.fixture(scope='function')
+def tables(engine):
+    """
+    Returns session-wide initialized database tables.
+    """
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
 
-    with app.app_context():
-        Base.metadata.drop_all(bind=engine)
-        SessionLocal.remove()
+
+@pytest.fixture(scope='function')
+def session(engine, tables, request):
+    """
+    Creates a new database session for a test.
+    """
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection, binds={})
+    session = SessionLocal(**options)
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        session.close()
+
+    request.addfinalizer(teardown)
+    return session
 
 
 @pytest.fixture(scope='function')
@@ -32,19 +60,3 @@ def client(app):
 @pytest.fixture(scope='function')
 def runner(app):
     return app.test_cli_runner()
-
-
-@pytest.fixture(scope='function')
-def session(app, request):
-    connection = SessionLocal().bind.connect()
-    transaction = connection.begin()
-    options = dict(bind=connection, binds={})
-    db_session = SessionLocal(**options)
-
-    def cleanup():
-        transaction.rollback()
-        connection.close()
-        db_session.close()
-
-    request.addfinalizer(cleanup)
-    return db_session
