1a0c66339e02ba9340247cc64f574f486f5a60a9
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-22 02:05:30 +0200

    config changes to use PostgreSQL on both dev and test DB

--- src/config.py
+++ src/config.py
@@ -5,28 +5,27 @@ load_dotenv()
 
 
 class Config:
-    """Base configuration."""
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'my_precious')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    """Development configuration."""
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f"postgresql+psycopg2://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('DB_HOST_IP')}:5432/{os.getenv('POSTGRES_DB')}")
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{os.environ.get('POSTGRES_USER')}:"
+        f"{os.environ.get('POSTGRES_PASSWORD')}@"
+        f"{os.environ.get('DB_HOST_IP')}:5432/{os.getenv('POSTGRES_DB')}"
+    )
 
 
 class TestingConfig(Config):
-    """Testing configuration."""
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{os.environ.get('POSTGRES_USER')}:"
+        f"{os.environ.get('POSTGRES_PASSWORD')}@"
+        f"{os.environ.get('DB_HOST_IP')}:5432/{os.getenv('TEST_POSTGRES_DB')}"
+    )
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
-config_by_name = dict(
-    development=DevelopmentConfig,
-    testing=TestingConfig,
-
-)
-
-key = Config.SECRET_KEY
+config_by_name = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
