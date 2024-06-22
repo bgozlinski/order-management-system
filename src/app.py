3781f9e98d3d24d3fd12099fd0fb4ab61f136308
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-06-23 01:27:54 +0200

    moved Blueprint to __init__ of endpoints.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from src.config import config_by_name
-from src.routes.endpoints import bp
-
+from src.routes.endpoints import api_orders_bp
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -16,6 +15,6 @@ def create_app(config_name):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    app.register_blueprint(bp, url_prefix='/api')
+    app.register_blueprint(api_orders_bp)
 
     return app
